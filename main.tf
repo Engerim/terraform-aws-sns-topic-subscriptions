e63e2fb5c7d5f3a9fee7df8153c278319e3b11a6
Alexander Miehe <alexander.miehe@flaconi.de>
2020-01-02 12:46:21 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 resource "aws_sns_topic_subscription" "this" {
-  for_each               = { for r in var.sns_topic_subscriptions : "${r.name}:${r.protocol}:${r.endpoint}" => r }
+  for_each               = { for r in var.sns_topic_subscriptions : "${r.name}:${r.protocol}:${r.endpoint}:${r.raw_message_delivery}" => r }
   topic_arn              = each.value.topic_arn
   protocol               = each.value.protocol
   endpoint               = each.value.endpoint
   endpoint_auto_confirms = each.value.endpoint_auto_confirms
+  raw_message_delivery   = lookup(each.value, "raw_message_delivery", null)
 }
